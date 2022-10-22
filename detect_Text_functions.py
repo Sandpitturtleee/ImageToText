7c1b5f3d1561d3fb6d2bb6d66a8463d5c6e253d3
Sandpitturtleee <karolmazur@onet.eu>
2022-10-23 00:41:38 +0200

    Two new .py files, crop, detect functions

---
+++ detect_Text_functions.py
@@ -0,0 +1,89 @@
+import cv2
+import pytesseract
+
+
+def detect_character(image):
+    image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    data = pytesseract.image_to_string(image, lang='eng', config='--psm 6 --oem 3')
+    print(data)
+    #cv2.imshow('thresh', image)
+
+
+def detect_lvl(image):
+    image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    data = pytesseract.image_to_string(image, lang='eng', config='--psm 6 --oem 3 -c tessedit_char_whitelist=0123456789')
+    print(data)
+    #cv2.imshow('thresh', image)
+
+
+def detect_nick(image):
+    image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    data = pytesseract.image_to_string(image, lang='eng', config='--psm 6 --oem 3')
+    print(data)
+    #cv2.imshow('thresh', image)
+
+
+def detect_id(image):
+    image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    data = pytesseract.image_to_string(image, lang='eng', config='--psm 6 --oem 3 -c tessedit_char_whitelist=0123456789')
+    print(data)
+    #cv2.imshow('thresh', image)
+
+
+def detect_bow_name(image):
+    image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    data = pytesseract.image_to_string(image, lang='eng', config='--psm 6 --oem 3')
+    print(data)
+    #cv2.imshow('thresh', image)
+
+
+def detect_set(image):
+    image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    data = pytesseract.image_to_string(image, lang='eng', config='--psm 6 --oem 3')
+    print(data)
+    #cv2.imshow('thresh', image)
+
+
+def detect_refinement(image):
+    image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    data = pytesseract.image_to_string(image, lang='eng', config='--psm 6 --oem 3 -c tessedit_char_whitelist=0123456789')
+    print(data)
+    #cv2.imshow('thresh', image)
+
+
+def detect_bow_lvl(image):
+    image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    data = pytesseract.image_to_string(image, lang='eng', config='--psm 6 --oem 3 -c tessedit_char_whitelist=0123456789')
+    print(data)
+    #cv2.imshow('thresh', image)
+
+
+def detect_fr_lvl(image):
+    image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    data = pytesseract.image_to_string(image, lang='eng', config='--psm 6 --oem 3 -c tessedit_char_whitelist=0123456789')
+    print(data)
+    #cv2.imshow('thresh', image)
+
+
+def detect_aa_lvl(image):
+    image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    data = pytesseract.image_to_string(image, lang='eng', config='--psm 6 --oem 3 -c tessedit_char_whitelist=0123456789')
+    print(data)
+    #cv2.imshow('thresh', image)
+
+
+def detect_e_lvl(image):
+    image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    data = pytesseract.image_to_string(image, lang='eng', config='--psm 6 --oem 3 -c tessedit_char_whitelist=0123456789')
+    print(data)
+    #cv2.imshow('thresh', image)
+
+
+def detect_q_lvl(image):
+    image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    data = pytesseract.image_to_string(image, lang='eng', config='--psm 6 --oem 3 -c tessedit_char_whitelist=0123456789')
+    print(data)
+    #cv2.imshow('thresh', image)
+
+
+
