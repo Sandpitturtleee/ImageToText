fb56dc681335093d2556ea1b6f84f96114418126
Sandpitturtleee <karolmazur@onet.eu>
2022-10-25 22:58:26 +0200

    Adding exception handling in functions in detect_Text_functions.py

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 import os
-
-from PIL import Image
-from pytesseract import pytesseract
-import cv2
-import pytesseract
-from detect_Text_functions import *
 from crop_functions import *
 from write_to_file_functions import *
 
-if __name__ == '__main__':
 
+def combine1():
     pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
     path_to_images = "C:/Users/karol/Desktop/Studia/5 Semestr/Programowanie Wieloplatformowe w Qt/ImageToText/venv/Images/Base/"
 
@@ -20,8 +14,12 @@ if __name__ == '__main__':
             dim = (1927, 804)
             resized = cv2.resize(image, dim)
             crop(resized, y)
-            y = y+1
-    write_to_file()
+            y = y + 1
+
+
+if __name__ == '__main__':
+    combine1()
+    write_to_file1()
 
 
 
--- write_to_file_functions.py
+++ write_to_file_functions.py
@@ -6,9 +6,9 @@ file_paths_strings = ["character", "lvl", "nick", "uid",
                       "hp_stat", "atk_stat", "def_stat", "em_stat",
                       "cr_stat", "cd_stat", "er_stat"]
 file_paths_strings_New = ["character", "lvl", "nick", "uid",
-                      "bow_name", "artifact", "refinement", "bow_lvl",
-                      "fr_lvl","hp_stat", "atk_stat", "def_stat", "em_stat",
-                      "cr_stat", "cd_stat", "er_stat"]
+                          "bow_name", "artifact", "refinement", "bow_lvl",
+                          "fr_lvl", "hp_stat", "atk_stat", "def_stat", "em_stat",
+                          "cr_stat", "cd_stat", "er_stat"]
 
 path_to_data = "C:/Users/karol/Desktop/Studia/5 Semestr/Programowanie Wieloplatformowe w Qt/ImageToText/venv/Data/"
 
@@ -17,21 +17,26 @@ data_lists = [character_list, lvl_list, nick_list, uid_list, bow_name_list, arti
               def_stat_list,
               em_stat_list, cr_stat_list, cd_stat_list, er_stat_list]
 data_lists_New = [character_list, lvl_list, nick_list, uid_list, bow_name_list, artifact_list, refinement_list,
-              bow_lvl_list, fr_lvl_list, hp_stat_list, atk_stat_list,
-              def_stat_list,
-              em_stat_list, cr_stat_list, cd_stat_list, er_stat_list]
+                  bow_lvl_list, fr_lvl_list, hp_stat_list, atk_stat_list,
+                  def_stat_list,
+                  em_stat_list, cr_stat_list, cd_stat_list, er_stat_list]
 
-#for line in data_lists:
-    #f.write("%s\n" % line)
 
-def write_to_file():
+# 1 With a_stat. e_stat, q_stat values
+def write_to_file1():
+    lists_len = len(character_list)
+    for x in range(len(file_paths_strings)):
+        file_paths_strings[x] = path_to_data + file_paths_strings[x] + ".txt"
+        with open(file_paths_strings[x], 'w') as f:
+            for y in range(lists_len):
+                f.write("%s\n" % data_lists[x][y])
+
+
+# 2 Without a_stat. e_stat, q_stat values
+def write_to_file2():
     lists_len = len(character_list)
     for x in range(len(file_paths_strings_New)):
         file_paths_strings_New[x] = path_to_data + file_paths_strings_New[x] + ".txt"
         with open(file_paths_strings_New[x], 'w') as f:
             for y in range(lists_len):
                 f.write("%s\n" % data_lists_New[x][y])
-
-
-
-
