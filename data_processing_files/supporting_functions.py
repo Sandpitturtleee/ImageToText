df0ba45e3320f36759815b8bfbf9ea2d16c50a2b
Sandpitturtleee <karolmazur@onet.eu>
2022-11-25 23:23:36 +0100

    Adding functions to read data from files

---
+++ data_processing_files/supporting_functions.py
@@ -0,0 +1,42 @@
+from variables import bow_name_file_path, element_type_file_path, refinement_file_path, element_value_file_path, \
+    artifact_file_path, bow_name, refinement, element_type, element_value, artifact
+
+
+def read_values():
+    read_bow_name()
+    read_refinement()
+    read_element_type()
+    read_element_value()
+    read_artifact()
+
+
+def read_bow_name():
+    with open(bow_name_file_path) as file:
+        for line in file:
+            bow_name.append(line)
+
+
+def read_refinement():
+    with open(refinement_file_path) as file:
+        for line in file:
+            refinement.append(line)
+
+
+def read_element_type():
+    with open(element_type_file_path) as file:
+        for line in file:
+            element_type.append(line)
+
+
+def read_element_value():
+    with open(element_value_file_path) as file:
+        for line in file:
+            element_value.append(line)
+
+
+def read_artifact():
+    with open(artifact_file_path) as file:
+        for line in file:
+            artifact.append(line)
+
+
--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import sys
 from PyQt5.QtWidgets import QApplication
 
 from pyQt_files.pyQt_MainMenu import MainMenuWindow
-
+from data_processing_files.supporting_functions import read_values
+from data_processing_files.data_manipulation import bow_searching
 
 if __name__ == '__main__':
+    read_values()
+    bow_searching()
     app = QApplication(sys.argv)
     win = MainMenuWindow()
     win.show()
